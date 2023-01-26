01645aa7653d7d08cdf12e448e875ec68ce214c3
SeijDeLeon <sdeleon@berkeley.edu>
2023-01-25 16:30:23 -0800

    Complete Adding Routing Callbacks

--- server/app.js
+++ server/app.js
@@ -17,61 +17,61 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/',
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/create',
-(req, res) => {
-  res.render('index');
-});
+  (req, res) => {
+    res.render('index');
+  });
 
 app.get('/links',
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
+      });
+  });
 
 app.post('/links',
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
     // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
+      return res.sendStatus(404);
+    }
 
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
         throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
 
 /************************************************************/
 // Write your authentication routes here
@@ -81,11 +81,11 @@ app.post('/links',
 app.post('/signup', (req, res, next) => {
   models.Users.create(req.body)
     .then((result)=> {
-      console.log('User Created  ', result);
+    // console.log('User Created  ', result);
       res.redirect(201, '/');
     })
     .catch((err) => {
-      console.log('User NOT Created    ', err);
+      //console.log('User NOT Created    ', err);
       //redirect to /signup
       res.redirect(302, '/signup');
     });
@@ -93,7 +93,35 @@ app.post('/signup', (req, res, next) => {
 
 
 app.post('/login', (req, res, next) => {
+//Run compare. Invoke on req.body.attempted
+//Get password and salt from database
+//.then (**LOGIN**)
+//.catch(redirect to login)
 
+  //{attempted:req.body.attempted, password: Invoke createpassword, salt: create salt}
+
+  models.Users.get({username: req.body.username})
+    .then((result)=> {
+      if (result === undefined) {
+        //
+        console.log('User does not exist ');
+        //redirect to /signup
+        res.redirect(302, '/login');
+      } else {
+        if (models.Users.compare(req.body.password, result.password, result.salt)) {
+          console.log('Logging In');
+          res.redirect(201, '/');
+        } else {
+          console.log('Invalid Login');
+          res.redirect(302, '/login');
+        }
+      }
+    })
+    .catch((err) => {
+      console.log('User does not exist    ', err);
+      //redirect to /signup
+      res.redirect(302, '/login');
+    });
 });
 
 
